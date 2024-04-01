43e2133b8cbcc0827de06337202050fb615a6469
ukyoung147 <ukyoung147@gmail.com>
2024-04-01 17:40:19 +0900

    Feat: #6 사용자 현재 위치를 고려한 관광지 추천
    - GeoDistance 기반 검색
    - 클라이언트의 현재 위치의 10km 이내에서 필터링

--- app/models/my_spot.py
+++ app/models/my_spot.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, BigInteger, String, ForeignKey
+from sqlalchemy import Column, BigInteger, String, ForeignKey, Date
 from sqlalchemy.orm import relationship
 from database import Base
 
@@ -7,9 +7,9 @@ class MySpot(Base):
     __tablename__ = "my_spots"
 
     id = Column(BigInteger, primary_key=True, index=True, autoincrement=True)
-    user_id = Column(BigInteger, ForeignKey('users.id'))
+    user_id = Column(BigInteger, ForeignKey("users.id"))
     spot_id = Column(BigInteger, nullable=False)
     memo = Column(String(255), nullable=True)
-    created_date = Column(String(255), nullable=True)
+    created_date = Column(Date, nullable=True)
 
     user = relationship("User", back_populates="my_spots")
