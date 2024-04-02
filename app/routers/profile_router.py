f0c5c6860cb27669f4313aa8f8f52227c2a1af2b
ukyoung147 <ukyoung147@gmail.com>
2024-04-02 13:29:45 +0900

    Merged by deploy

--- app/routers/profile_router.py
+++ app/routers/profile_router.py
@@ -41,7 +41,9 @@ async def get_user_profile(
 
     user_data = UserInfoResponse(
         username=user.nickname,
-        profileImage=None if user.profile_image == "profiles/defaultProfile.svg" else user.profile_image,
+        profileImage=None
+        if user.profile_image == "profiles/defaultProfile.svg"
+        else user.profile_image,
     )
     return user_data
 
