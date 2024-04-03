26d7cb24f47bc10b9da1c904f6fc375fa09fd2a7
Chosamee <cytor@naver.com>
2024-04-03 14:11:21 +0900

    fix: Fix 비회원 raise 401

--- app/services/profile.py
+++ app/services/profile.py
@@ -6,8 +6,8 @@ from fastapi import HTTPException, Request
 async def get_internal_id(request: Request):
     internal_id = request.headers.get("INTERNAL_ID_HEADER")
     if not internal_id:
-        return "8b5b03b7-ae9f-458e-a2b9-558eac541629"
-        # raise HTTPException(status_code=400, detail="Internal ID not provided")
+        # return "8b5b03b7-ae9f-458e-a2b9-558eac541629"
+        raise HTTPException(status_code=400, detail="Internal ID not provided")
     return internal_id
 
 
